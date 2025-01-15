766e0d684bc63783e230ecfd85fab9350a52280d
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-01-14 22:56:32 -0800

    Completed Question 4

--- q4.js
+++ q4.js
@@ -3,7 +3,18 @@ A palindrome is a word, number, or other sequence of characters that reads the s
 */
 
 function checkPalindrome(textInput) {
+    let lastLetter = textInput.length - 1;
     
+    for (let i = 0; i < textInput.length; i++) {
+        if (lastLetter === 1) {
+            return console.log(true)
+        }
+        else if (textInput.charAt(lastLetter) === textInput.charAt(i)) {
+            lastLetter--;
+        }
+    }
+
+    return console.log(false)
 }
 
 // Test for palindrome
@@ -12,3 +23,5 @@ let testString = "racecar"; // true
 let testString2 = "hello"; // false
 
 // TODO: display the result of the test strings in the console
+checkPalindrome(testString);
+checkPalindrome(testString2);
